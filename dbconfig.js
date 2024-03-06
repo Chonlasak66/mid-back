a96559f1b10ce4a725915a31e72e4e9a4114d8f1
chonlasak <chonlasak@rmutl.ac.th>
2024-03-06 14:05:31 +0700

    server fix

--- dbconfig.js
+++ dbconfig.js
@@ -12,7 +12,7 @@ var dbconfig = {
         host: 'localhost',
         port: '3306',
         user: 'dbuser66',
-        password: 'P@ssw0rd@2023',
+        password: 'P@ssw0rd',
         database: 'user66008_db'
     }
 };
