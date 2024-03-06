94f1102c728945803db5b188b90e64318e322fd8
chonlasak <chonlasak@rmutl.ac.th>
2024-03-06 10:05:47 +0700

    port change

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const port = 5000;
+
+const app = express();
+
 require('dotenv').config();
 const Server = require('./models/server');
 const server = new Server();
 
 server.listen();
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
