0a5835301d0a87b071a8cb7cfd38ae8048a246e8
chonlasak <chonlasak@rmutl.ac.th>
2024-03-06 10:01:35 +0700

    port change

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 const Server = require('./models/server');
 const server = new Server();
 
-server.listen();
+server.listen(3008, () => {
+    console.log('Server started on port 3008');
+});
