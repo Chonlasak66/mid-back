02431ece4ab88fd5d67a5062f4628d3d5994f410
chonlasak <chonlasak@rmutl.ac.th>
2024-03-08 10:52:19 +0700

    server fix

--- controllers/saveuser.js
+++ controllers/saveuser.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path'); 
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+// saveuser.js
 
+const mysql = require('mysql');
 const env = require('../env.js');
 const config = require('../dbconfig.js')[env];
 
 const connection = mysql.createConnection({
   host: config.host,
   user: config.user,
+  port: config.port,
   password: config.password,
   database: config.database
-})
+});
+
 connection.connect((err) => {
   if (err) {
       console.error('Error connecting to MySQL:', err);
@@ -21,30 +20,6 @@ connection.connect((err) => {
   }
 });
 
-console.log('Running Environment: ' + env);
-
-const app = express();
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-});
-
-app.get('/users', (req, res) => {
-    axios.get('https://randomuser.me/api/?page=1&results=10')
-        .then(response => {
-            res.send(response.data);
-         });
-});
-
-app.post('/saveuser', (req, res) => {
-  const userData = req.body;
-  console.log(userData);
-  saveUserData(userData);
-});
-
-// ฟังก์ชันสำหรับบันทึกข้อมูลผู้ใช้ในฐานข้อมูล
 function saveUserData(userData) {
   const { gender, name, location, email, login, picture ,dob} = userData;
   const { title, first, last } = name;
@@ -55,8 +30,6 @@ function saveUserData(userData) {
 
   console.log(userData);
 
-  // const sql = `INSERT INTO users (gender, title, first, last, country,dob, uuid, email, username, password, picture_large, picture_medium, picture_thumbnail) 
-  //              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   const sql = 'INSERT INTO users SET ?';
   const values = {gender, title, first, last, country,dob:dateOfBirth, uuid, email, username, password, md5, sha1, sha256, picture_large:large, picture_medium:medium, picture_thumbnail:thumbnail};
 
@@ -68,7 +41,4 @@ function saveUserData(userData) {
   });
 }
 
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+module.exports = saveUserData;
